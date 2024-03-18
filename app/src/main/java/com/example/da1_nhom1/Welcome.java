f8ae7f34f960dadea60bab32a902767630078a79
Dung24 <senpai6868@gmail.com>
2024-03-18 09:46:41 +0700

    add

--- app/src/main/java/com/example/da1_nhom1/Welcome.java
+++ app/src/main/java/com/example/da1_nhom1/Welcome.java
@@ -20,5 +20,6 @@ public class Welcome extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
